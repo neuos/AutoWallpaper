4e47647e75da471a0389cfcea17619b76a25266e
neuos <github@neuhuber.eu>
2026-04-14 23:14:36 +0200

    update dependencies and clean up activity metadata

--- app/src/main/java/eu/neuhuber/autowallpaper/ui/theme/Theme.kt
+++ app/src/main/java/eu/neuhuber/autowallpaper/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package eu.neuhuber.autowallpaper.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
