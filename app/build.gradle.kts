0b8f5bcb74af851a4b513ba7f5d8c882378410a0
neuos <github@neuhuber.eu>
2026-04-14 23:56:09 +0200

    upgrade compile sdk to 37 Cinnamon Bun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "eu.neuhuber.autowallpaper"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "eu.neuhuber.autowallpaper"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
