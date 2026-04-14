f0e37fde2e4150d8f4f3e1f0dbf9747f34c92ed4
neuos <github@neuhuber.eu>
2026-04-15 01:39:40 +0200

    fix worker init

--- app/src/main/java/eu/neuhuber/autowallpaper/AutoWallpaperApp.kt
+++ app/src/main/java/eu/neuhuber/autowallpaper/AutoWallpaperApp.kt
@@ -1,14 +1,23 @@
 package eu.neuhuber.autowallpaper
 
 import android.app.Application
+import androidx.work.Configuration
 import eu.neuhuber.autowallpaper.di.appModule
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.workmanager.factory.KoinWorkerFactory
 import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
-class AutoWallpaperApp : Application() {
+class AutoWallpaperApp : Application(), Configuration.Provider {
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(getKoin().get<KoinWorkerFactory>())
+            .build()
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
